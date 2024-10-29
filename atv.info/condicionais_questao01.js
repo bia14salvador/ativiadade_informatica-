ba5ec8a0188c419ef5107748748ea88a9dfb6d81
bia14salvador <144938853+bia14salvador@users.noreply.github.com>
2024-10-28 22:05:13 -0300

    condicionais_questao01.js

---
+++ atv.info/condicionais_questao01.js
@@ -0,0 +1,8 @@
+const numero1 = 96; 
+const numero2 = 92; 
+
+if (numero1 > numero2) {
+  console.log(`O número ${numero1} é maior que ${numero2}.`);
+}
+ else if (numero1 < numero2) 
+  console.log(`O número ${numero2} é maior que ${numero1}.`);
