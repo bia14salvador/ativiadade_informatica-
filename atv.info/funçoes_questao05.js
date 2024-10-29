8f6366461a48e526088bd87f25ae4ab9e3195084
bia14salvador <144938853+bia14salvador@users.noreply.github.com>
2024-10-28 22:13:38 -0300

    funçoes_questao05.js

---
+++ atv.info/funçoes_questao05.js
@@ -0,0 +1,13 @@
+function encontrandoMaiorPalavra(frase) {
+  const palavras = frase.split(' ');
+
+  let maiorPalavra = '';
+
+  for (let palavra of palavras) {
+    if (palavra.length > maiorPalavra.length) {
+      maiorPalavra = palavra; 
+    }
+  }
+
+  return maiorPalavra; 
+}
