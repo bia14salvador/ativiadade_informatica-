ef3f918903bc1c30f15dc5cb2e9bb69b66687c97
bia14salvador <144938853+bia14salvador@users.noreply.github.com>
2024-10-28 21:53:31 -0300

    fundamentos_questao05.js

---
+++ atv.info/fundamentos_questao05.js
@@ -0,0 +1,9 @@
+const num1 = 51; 
+const num2 = 70; 
+const num3 = 60; 
+
+if (num1 >= 50 && num1 <= 99 && num2 >= 50 && num2 <= 99 && num3 >= 50 && num3 <= 99) {
+    console.log("Sucesso"); 
+} else {
+    console.log("Erro"); 
+}
